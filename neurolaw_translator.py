05bcfc9ed9a33128708bf09473b4ca73de85a3b0
Carlo Marcelo Revoredo da Silva <revoredo@gmail.com>
2021-12-25 21:43:23 -0300

    Update neurolaw_translator.py

--- neurolaw_translator.py
+++ neurolaw_translator.py
@@ -1,8 +1,4 @@
 import re
 
 def temporal_expressions_pt_to_eng(text_in_pt):
-
     return "teste"
-
-#str = "o primeiro dia do mês de fevereiro de 2022"
-#print(temporal_expressions_pt_to_eng(str))
